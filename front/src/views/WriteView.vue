c39ae54e87cb825493668ff0a26567e5bae01129
swkim0911 <qqazws7@konkuk.ac.kr>
2024-08-14 14:40:02 +0900

    init

---
+++ front/src/views/WriteView.vue
@@ -0,0 +1,11 @@
+<script setup>
+import { ref } from 'vue'
+
+const count = ref(0)
+</script>
+<style></style>
+<template>
+  <p>안녕하세요</p>
+  <button @click="count += 1">버튼을 눌러주세요</button>
+  <p>Count: {{ count }}</p>
+</template>
